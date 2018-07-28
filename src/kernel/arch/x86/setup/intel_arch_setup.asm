2d3b5965340bb8149b8e5519bcb988d4e6369bbe
JJL772 <jeremy.lorelli.1337@gmail.com>
2018-07-28 01:06:48 -0700

    Add no-op instruction to empty functions

--- src/kernel/arch/x86/setup/intel_arch_setup.asm
+++ src/kernel/arch/x86/setup/intel_arch_setup.asm
@@ -23,77 +23,110 @@ section .text
 	IVYBRIDGE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Ivybridge setup code here
+		nop
+
 		pop ebp
 		ret
 
 	CANNONLAKE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Cannonlake setup code here
+		nop
+
 		pop ebp
 		ret
 
 	COFFEELAKE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Coffeelake setup code here
+		nop
+
 		pop ebp
 		ret
 
 	KABYLIKE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Kabylake setup code here
+		nop
+
 		pop ebp
 		ret
 
 	SKYLAKE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Skylake setup code here
+		nop
+
 		pop ebp
 		ret
 
 	BROADWELL_SETUP:
 		push ebp
 		mov esp, ebp
+		
 		;Broadwell setup code here
+		nop
+
 		pop ebp
 		ret
 
 	HASWELL_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Haswell setup code here
+		nop
+
 		pop ebp
 		ret
 
 	SANDYBRIDGE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Sandybridge setup code here
+		nop
+
 		pop ebp
 		ret
 
 	WESTMERE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Westmere setup code here
+		nop
+
 		pop ebp
 		ret
 
 	NEHALEM_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Nehalem setup code here
+		nop
+
 		pop ebp
 		ret
 
 	PENRYN_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Penryn setup code here
+		nop
+
 		pop ebp
 		ret
 
@@ -101,41 +134,59 @@ section .text
 	CORE_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Core setup code
+		nop
+
 		pop ebp
 		ret
 
 	MOD_PENTIUMM_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Modified pentium M setup code here
+		nop
+
 		pop ebp
 		ret
 
 	PENTIUMM_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;Pentium M setup code here
+		nop
+
 		pop ebp
 		ret
 
 	P6_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;P6 setup code here
+		nop
+
 		pop ebp
 		ret
 
 	I486_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;i486 setup code here
+		nop
+
 		pop ebp
 		ret
 
 	I386_SETUP:
 		push ebp
 		mov esp, ebp
+
 		;i386 setup code here
+		nop
+
 		pop ebp
 		ret
