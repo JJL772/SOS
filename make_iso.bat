f2320cbc158ae6298506c2f338da0086095f3051
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-03-16 00:34:34 -0700

    Remove old files

--- make_iso.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Generating disk image...
-echo Settings: -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -A os -input-charset utf8 -quiet -boot-info-table -o os.iso iso
-bash -c "genisoimage -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -A os -input-charset utf8 -quiet -boot-info-table -o build/iso/os.iso iso"
-echo Done!
-PAUSE
--- run_vm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3.7 "scripts/run_vm.py"
