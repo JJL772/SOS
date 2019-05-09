78b7e2c25bcfd5023668272a6e3dcad5b4c604fd
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-05-08 22:40:55 -0700

    Add some common headers

---
+++ src/common/intrin.h
@@ -0,0 +1,12 @@
+//===============================================//
+// Name: intrin.h
+// Purpose: General purpose intrinsics
+// Author: Jeremy Lorelli jeremy.lorelli.1337@gmail.com
+//===============================================//
+#ifndef _INTRIN_H_
+#define _INTRIN_H_
+
+
+
+#endif //_INTRIN_H_
+
---
+++ src/common/simdintrin.h
@@ -0,0 +1,11 @@
+//===============================================//
+// Name: simdintrin.h
+// Purpose: SIMD intrinsics
+// Author: Jeremy Lorelli jeremy.lorelli.1337@gmail.com
+//===============================================//
+#ifndef _SIMDINTRIN_H_
+#define _SIMDINTRIN_H_
+
+
+
+#endif //_SIMDINTRIN_H_
